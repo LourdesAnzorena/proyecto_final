68815746f0d4ed913d7f3f607b4c7c49d75fc389
Lourdes Anzorena <lourdesanzorena97@gmail.com>
2024-12-11 18:48:30 -0300

    Descripción de los cambios realizados

---
+++ menu.py
@@ -0,0 +1,7 @@
+from Proyecto_final.cliente import Cliente
+from Proyecto_final.primera_entrega import bd_info_usuario
+
+Cliente1=Cliente(1,'Lourdes','Anzorena','Mendoza','2615392694','lourdesanzorena97@gmail.com',40873217)
+print(Cliente1)
+
+print(bd_info_usuario)
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="proyecto_final",
+    version="1.0",
+    description="Paquete para proyecto final Python.",
+    author="Lourdes Anzorena",
+    author_email="lourdesanzorena97@gmail.com",
+    packages=["Proyecto_final"]
+)
+
+
